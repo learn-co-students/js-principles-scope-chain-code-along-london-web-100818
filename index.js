f32c418ac98c3898146b992d3b3c68bed36450c3
ju-zp <jhuntington@tutanota.com>
2018-11-19 10:51:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 /* Your code goes here */
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "a thing"
+}
+
+function reassignLeastFavorite(){
+    leastFavoriteCustomer = "somethong else";
+}
+
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = "someone";
+    let favoriteCustomer = "someone else";
+}
